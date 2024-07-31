d28c67e14916b3da273e7db7fed97bc39e972bde
Akash Patil <110560351+akashpatil0@users.noreply.github.com>
2024-07-30 21:22:37 -0400

    Added support for autodeploy via github actions. Cleaned up files.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import "./App.css";
 
 const App = () => {
-    return (
-        <div className="App">
-            <header className="App-header">App</header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">App</header>
+    </div>
+  );
 };
 
 export default App;
